c250b807463a28a261bdec38b7059b22b85b5766
Rafael Plinio <racpsjcsp@gmail.com>
2023-12-07 18:24:00 -0300

    change new UI on PasswordGeneratorView

--- Passwordr/Passwordr/Views/PasswordGeneratorView/PasswordGeneratorView.swift
+++ Passwordr/Passwordr/Views/PasswordGeneratorView/PasswordGeneratorView.swift
@@ -21,104 +21,84 @@ struct PasswordGeneratorView: View {
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                Text(K.Strings.passwordHowManyCharacters)
-                    .foregroundStyle(.purple)
-                    .font(.title2)
-
-                Slider(value: $charCount, in: 1...20, step: 1) {
-                } minimumValueLabel: {
-                    Text("1")
-                        .font(.title2)
-                } maximumValueLabel: {
-                    Text("20")
-                        .font(.title2)
-                } onEditingChanged: { editing in
-                    isEditing = editing
-                }
-
-                Text("\(Int(charCount))")
-                    .foregroundColor(isEditing ? Color(K.Strings.myGreen) : .purple)
-                    .font(.title2)
-                    .padding(.top, -16)
-
-                VStack {
-                    Toggle(isOn: $isSymbolsOn) {
-                        Text(K.Strings.passwordIncludeSymbol)
-                            .foregroundStyle(Color(K.Strings.myTeal))
-                            .font(.title2)
-                    }
-                    .padding(.bottom, 20)
-
-                    Toggle(isOn: $isNumbersOn) {
-                        Text(K.Strings.passwordIncludeNumber)
-                            .foregroundStyle(Color(K.Strings.myYellow))
-                            .font(.title2)
-                    }
-                    .padding(.bottom, 20)
-
-                    Toggle(isOn: $isEmptySpaceOn) {
-                        Text(K.Strings.passwordIncludeEmptySpace)
-                            .font(.title2)
-                    }
-                    .padding(.bottom, 20)
-
-                    Toggle(isOn: $hasUpperCase) {
-                        Text(K.Strings.passwordIncludeUpperCase)
-                            .foregroundStyle(Color(K.Strings.myRed))
-                            .font(.title2)
-                    }
-                }
-                .padding()
-                .padding(.bottom, 30)
-
-                Button {
-                    characters = viewModel.generatePassword(lenght: Int(charCount), specialCharacters: isSymbolsOn, uppercase: hasUpperCase, numbers: isNumbersOn, emptySpace: isEmptySpaceOn)
-                    generatedPassword = characters.joined()
-                } label: {
-                    Image(systemName: K.Strings.wandAndStars)
-                        .imageScale(.large)
-                    Text(K.Strings.generatePassword)
-                        .font(.title3)
-                }
-                .buttonStyle(.plain).controlSize(.regular)
-                .foregroundStyle(Color(K.Strings.myGreen))
-                .clipShape(.buttonBorder)
-                .padding()
-                .overlay(RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color(K.Strings.myGreen), lineWidth: 1))
-
-                HStack(spacing: 0.5) {
-                    if generatedPassword.isEmpty {
-                        Text(K.Strings.password)
-                            .foregroundStyle(Color(K.Strings.myGray))
-                            .padding(.top, 20)
-                    } else {
-                        ForEach(characters, id: \.self) { character in
-                            Text(character)
-                                .foregroundColor(viewModel.specialCharactersArray.contains(character) ? Color(K.Strings.myTeal) : viewModel.numbersArray.contains(character) ? Color(K.Strings.myYellow) : viewModel.alphabet.contains(character) ? Color(K.Strings.myWhite) : Color(K.Strings.myRed))
-                                .padding(.top, 20)
+            ZStack {
+                Form {
+                    Section {
+                        Slider(value: $charCount, in: 1...20, step: 1) {
+                        } minimumValueLabel: {
+                            Text("1")
+                        } maximumValueLabel: {
+                            Text("20")
+                        } onEditingChanged: { editing in
+                            isEditing = editing
                         }
+                    } header: {
+                        Text(K.Strings.passwordHowManyCharacters) +
+                        Text(" \(Int(charCount))").foregroundStyle(.myGray).font(.body).bold()
+                    }
+
+                    Section {
+                        Toggle(isOn: $isSymbolsOn) {
+                            Text(K.Strings.passwordIncludeSymbol)
+                                .foregroundStyle(Color(K.Strings.myBlue))
+                        }.toggleStyle(.switch).tint(Color(K.Strings.myBlue))
+
+                        Toggle(isOn: $isNumbersOn) {
+                            Text(K.Strings.passwordIncludeNumber)
+                                .foregroundStyle(Color(K.Strings.myOrange))
+                        }.toggleStyle(.switch).tint(Color(K.Strings.myOrange))
+
+                        Toggle(isOn: $hasUpperCase) {
+                            Text(K.Strings.passwordIncludeUpperCase)
+                                .foregroundStyle(Color(K.Strings.myRed))
+                        }.toggleStyle(.switch).tint(Color(K.Strings.myRed))
+
+                        Toggle(isOn: $isEmptySpaceOn) {
+                            Text(K.Strings.passwordIncludeEmptySpace)
+                                .foregroundStyle(Color(K.Strings.myPurple))
+                        }.toggleStyle(.switch).tint(Color(K.Strings.myPurple))
+                    }
+
+                    Button {
+                        characters = viewModel.generatePassword(lenght: Int(charCount), specialCharacters: isSymbolsOn, uppercase: hasUpperCase, numbers: isNumbersOn, emptySpace: isEmptySpaceOn)
+                        generatedPassword = characters.joined()
+                    } label: {
+                            Image(systemName: K.Strings.wandAndStars)
+                                .imageScale(.large)
+                            Text(K.Strings.generatePassword)
+                    }
+                    .buttonStyle(.borderless)
+                    .frame(maxWidth: .infinity, maxHeight: 0, alignment: .center)
+                    .padding()
+
+                    Section {
+                        HStack(spacing: 0.5) {
+                            if generatedPassword.isEmpty {
+                                Text(K.Strings.password)
+                                    .foregroundStyle(Color(K.Strings.myGray))
+                            } else {
+                                ForEach(characters, id: \.self) { character in
+                                    Text(character)
+                                        .foregroundColor(viewModel.specialCharactersArray.contains(character) ? Color(K.Strings.myBlue) : viewModel.numbersArray.contains(character) ? Color(K.Strings.myOrange) : viewModel.alphabet.contains(character) ? Color(K.Strings.myWhite) : Color(K.Strings.myRed))
+                                }
+                            }
+
+                            Spacer()
+
+                            Button {
+                                print("tapped copy")
+                            } label: {
+                                Image(systemName: K.Strings.docOndoc)
+                                    .imageScale(.large)
+                            }
+                            .buttonStyle(.borderless).controlSize(.large)
+                        }
+                        .animation(.easeOut, value: 0.2)
                     }
                 }
-                .font(.title3)
-                .animation(.easeOut, value: 0.2)
-
-                Button {
-                    print("tapped copy")
-
-                } label: {
-                    Image(systemName: K.Strings.docOndoc)
-                        .imageScale(.large)
-                    Text(K.Strings.copy)
-                        .font(.title3)
-                }
-                .buttonStyle(.borderless).controlSize(.small)
-                .padding()
             }
             .navigationTitle(K.Strings.passwordGeneratorNavTitle)
             .navigationBarTitleDisplayMode(.inline)
-            .padding()
         }
     }
 }
